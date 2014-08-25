3c977f3853e43df1a1540b3543bbeed8abe89405
= <jshurak@gmail.com>
2014-08-24 20:45:56 -0400

    initial commit some silly db connections

---
+++ Instance.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CMS_C
+{
+    class Instance
+    {
+        public string instanceName { get; set; }
+        public string edition { get; set; }
+        public string version { get; set; }
+        public bool isClustered { get; set; }
+        public long maxMemory { get; set; }
+        public int minMemory { get; set; }
+        public string productLevel { get; set; }
+        public bool pingStatus { get; set; }
+
+
+        public Instance(string InstanceName)
+        {
+            instanceName = InstanceName;
+        }
+
+        public bool TestConnection()
+        {
+
+            SqlConnectionStringBuilder connectionstring = new SqlConnectionStringBuilder();
+            connectionstring["Data Source"] = instanceName;
+            connectionstring["Integrated Security"] = true;
+            connectionstring["Connect Timeout"] = 3;
+
+            SqlConnection conn = new SqlConnection(connectionstring.ConnectionString);
+
+            try
+            {
+                conn.Open();
+                if(conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                    return true;
+                }
+
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+                
+            }
+            return false;
+            
+        }
+    }
+}
