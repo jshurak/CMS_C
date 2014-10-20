da5aa29779dc2b34c48a699e91d482a4ca645ea3
Jeff Shurak <jshurak@gmail.com>
2014-10-20 14:20:06 -0400

    creating agentjob struct

---
+++ AgentJob.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS_C
+{
+    public struct AgentJob
+    {
+        private string _jobName;
+        private string _jobGUID;
+        private int _instanceID;
+
+        public string JobName 
+        { 
+            get
+            {
+                return _jobName;
+            }
+        
+        }
+
+        public string JobGUID
+        {
+            get
+            {
+                return _jobGUID;
+            }
+        }
+
+        public int InstanceID
+        {
+            get
+            {
+                return _instanceID;
+            }
+        }
+
+    }
+}
