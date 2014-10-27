0369b336c95bedd4f465e39d60f83f2e85172e41
Jeff Shurak <jshurak@gmail.com>
2014-10-27 08:56:43 -0400

    daily run check and timer setup

--- Service.cs
+++ Service.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.ServiceProcess;
 using System.Diagnostics;
 using System.Timers;
+using System.Data;
 
 namespace CMS_C
 {
@@ -23,6 +24,8 @@ namespace CMS_C
             this.CanStop = true;
         }
 
+        DateTime _lastDailyExecutionDateTime;
+        Timer _dailyTimer = new Timer(1000 * 60 * 60 * 24);
         
         /// <summary>
         /// Dispose of objects that need it here.
@@ -52,8 +55,15 @@ namespace CMS_C
 
             Timer _fiveMinuteTimer = new Timer(300000);
             Timer _thirtyMinuteTimer = new Timer(1000 * 60 * 30);
-            Timer _dailyTimer = new Timer(1000 * 60 * 60 * 24);
             
+
+            _lastDailyExecutionDateTime = GatherLastDailyExecution();
+            if(DateTime.Compare(_lastDailyExecutionDateTime, DateTime.Now) > 1)
+            {
+                Jobs.Daily(cache.ServerCache, cache.InstanceCache, cache.DatabaseCache, cache.AgentJobCache);
+            }
+
+            SetUpTimer(new TimeSpan(10, 00, 00), cache.ServerCache, cache.InstanceCache, cache.DatabaseCache, cache.AgentJobCache);
 
             _fiveMinuteTimer.Enabled = true;
             _fiveMinuteTimer.Start();
@@ -63,9 +73,30 @@ namespace CMS_C
             _thirtyMinuteTimer.Start();
             _thirtyMinuteTimer.Elapsed += new ElapsedEventHandler((sender, e) => ThirtyMinuteEvent(sender, e,cache.DatabaseCache,cache.InstanceCache,cache.AgentJobCache));
 
+            
+        }
+
+
+        private void SetUpTimer(TimeSpan alertTime, List<Server> ServerList, List<Instance> InstanceList, List<Database> DatabaseList, List<AgentJob> AgentList)
+        {
+            DateTime current = DateTime.Now;
+            TimeSpan timeToGo = alertTime - current.TimeOfDay;
+            if (timeToGo < TimeSpan.Zero)
+            {
+                return;//time already passed
+            }
+            System.Threading.Timer timer = new System.Threading.Timer(x =>
+            {
+                this.SetUpDaily(ServerList,InstanceList,DatabaseList,AgentList);
+            }, null, timeToGo, System.Threading.Timeout.InfiniteTimeSpan);
+        }
+
+        private void SetUpDaily(List<Server> ServerList,List<Instance> InstanceList, List<Database> DatabaseList,List<AgentJob> AgentList)
+        {
+            Jobs.Daily(ServerList, InstanceList, DatabaseList, AgentList);
             _dailyTimer.Enabled = true;
             _dailyTimer.Start();
-            _dailyTimer.Elapsed += new ElapsedEventHandler((sender, e) => DailyEvent(sender, e,cache.ServerCache,cache.InstanceCache,cache.DatabaseCache,cache.AgentJobCache));
+            _dailyTimer.Elapsed += new ElapsedEventHandler((sender, e) => DailyEvent(sender, e, ServerList, InstanceList, DatabaseList, AgentList));
         }
 
 
@@ -89,6 +120,25 @@ namespace CMS_C
             EventLogger.LogEvent("CMS Daily Job complete.", "Information");
         }
         
+        private DateTime GatherLastDailyExecution()
+        {
+            DateTime _lastDateTime = new DateTime();
+
+            DataSet _lastDailyExecution = Jobs.ConnectRepository("select top 1 StartTime from CollectionLog where ModuleName = 'Daily' order by LogID desc");
+            if (Jobs.TestDataSet(_lastDailyExecution))
+            {
+                foreach (DataRow pRow in _lastDailyExecution.Tables[0].Rows)
+                {
+                    _lastDateTime = (DateTime)pRow["StartTime"];
+                }
+            }
+            else 
+            {
+                _lastDateTime = Convert.ToDateTime("01/01/1900");
+            }
+
+            return _lastDateTime;
+        }
 
         /// <summary>
         /// OnStop(): Put your stop code here
